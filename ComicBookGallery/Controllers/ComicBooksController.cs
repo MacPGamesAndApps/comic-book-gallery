3786c7762d33826afebbe5cb4672ae0185954a25
MacPGamesAndApps <pcypc@hotmail.com>
2020-07-08 23:35:04 -0400

    Added Comic Books Detail view

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,12 +10,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if(DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("<h1>Hello World!</h1>");
+            return View();
         }
     }
 }
