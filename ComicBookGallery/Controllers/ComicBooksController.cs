b9ee400cf3f8faabab597732978d66d92b6339a2
MacPGamesAndApps <pcypc@hotmail.com>
2020-07-15 00:05:49 -0400

    Updated controller method to use repository.

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using ComicBookGallery.Models;
+﻿using ComicBookGallery.Data;
+using ComicBookGallery.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,23 +10,9 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Detail()
+        public ActionResult Detail(int id)
         {
-            ComicBook comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist() {Role = "Script", Name = "Dan Slott" },
-                    new Artist() {Role = "Pencils", Name = "Humberto Ramos" },
-                    new Artist() {Role = "Inks", Name = "Victor Olazaba" },
-                    new Artist() {Role = "Colors", Name = "Edgar Delgado" },
-                    new Artist() {Role = "Letters", Name = "Chris Eliopoulos" }
-                },
-                Favourite = true
-            };
+            ComicBook comicBook = new ComicBookRepository().GetComicBook(id);
 
             return View(comicBook);
         }
